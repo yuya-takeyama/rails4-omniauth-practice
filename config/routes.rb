2553df7cda2be898c0396227b700ffd6469929f3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-06-23 13:32:02 +0900

    Add HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails4OmniauthPractice::Application.routes.draw do
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
