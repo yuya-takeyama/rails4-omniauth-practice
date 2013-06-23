8486ba054b26862bc82a4103332f51384f998b71
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-06-23 14:08:10 +0900

    rails g devise:install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails4OmniauthPractice::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'rails4-omniauth-practice' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails4OmniauthPractice::Application.routes.draw do
+  root :to => 'home#index'
+
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
