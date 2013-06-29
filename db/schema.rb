2c12c619e01e42bf88bf8c36352430c1db573d8e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-06-29 21:25:54 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130629121803) do
+ActiveRecord::Schema.define(version: 20130629122432) do
 
   create_table "album_artists", force: true do |t|
     t.integer  "album_id"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20130629121803) do
 
   create_table "artists", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "album_id"
+    t.text     "body"
+    t.float    "point"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
