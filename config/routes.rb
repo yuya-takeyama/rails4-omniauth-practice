c20de20eeb3f07066624ee61583e3b9d18319789
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-06-29 20:56:31 +0900

    Add rails g scaffold Artist name:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails4OmniauthPractice::Application.routes.draw do
+  resources :artists
+
   resources :albums
 
   devise_for :users, :path => 'account', :controllers => { :omniauth_callbacks => "account/omniauth_callbacks" }
